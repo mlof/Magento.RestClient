5127bbb10ad99d50eabd662ba13f1494b655fcfd
Marcel <marcelrlof@gmail.com>
2021-09-02 09:08:26 +0200

    Re-added test configuration.

---
+++ source/Magento.RestClient.Tests.Configuration/TestConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Magento.RestClient.Tests.Configuration
+{
+	public class TestConfiguration
+	{
+		public string ConsumerKey { get; set; }
+		public string ConsumerSecret { get; set; }
+		public string AccessTokenSecret { get; set; }
+		public string AccessToken { get; set; }
+
+		public static TestConfiguration GetInstance()
+		{
+			var configurationRoot = new ConfigurationBuilder().AddUserSecrets<TestConfiguration>().Build();
+
+			var configuration = new TestConfiguration();
+			configurationRoot.Bind(configuration);
+			return configuration;
+		}
+	}
+}

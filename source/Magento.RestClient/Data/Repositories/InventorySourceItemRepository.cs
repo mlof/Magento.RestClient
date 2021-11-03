f765c47faa0fe1fe846908294f83e48b51285d00
Marcel <marcelrlof@gmail.com>
2021-11-03 15:17:49 +0100

    Implemented InventorySourceItemRepository

--- source/Magento.RestClient/Data/Repositories/InventorySourceItemRepository.cs
+++ source/Magento.RestClient/Data/Repositories/InventorySourceItemRepository.cs
@@ -1,8 +1,11 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using Magento.RestClient.Abstractions;
 using Magento.RestClient.Abstractions.Abstractions;
 using Magento.RestClient.Abstractions.Repositories;
 using Magento.RestClient.Data.Models.Inventory;
+using Magento.RestClient.Expressions;
+using RestSharp;
 
 namespace Magento.RestClient.Data.Repositories
 {
@@ -15,17 +18,35 @@ namespace Magento.RestClient.Data.Repositories
 
 		public IQueryable<InventorySourceItem> AsQueryable()
 		{
-			throw new System.NotImplementedException();
+			return new MagentoQueryable<InventorySourceItem>(Client, "inventory/source-items");
 		}
 
-		public void Create(params InventorySourceItem[] items)
+		/// <summary>
+		/// Create
+		/// </summary>
+		/// <param name="items"></param>
+		/// <returns></returns>
+		/// <exception cref="Magento.RestClient.Exceptions.Generic.MagentoException">Ignore.</exception>
+		public Task Create(params InventorySourceItem[] items)
 		{
-			throw new System.NotImplementedException();
+			var request = new RestRequest("inventory/source-items") {Method = Method.POST};
+			request.AddJsonBody(new {sourceItems = items});
+
+			return ExecuteAsync(request);
 		}
 
-		public void Delete(params InventorySourceItem[] items)
+		/// <summary>
+		/// Delete
+		/// </summary>
+		/// <param name="items"></param>
+		/// <returns></returns>
+		/// <exception cref="Magento.RestClient.Exceptions.Generic.MagentoException">Ignore.</exception>
+		public Task Delete(params InventorySourceItem[] items)
 		{
-			throw new System.NotImplementedException();
+			var request = new RestRequest("inventory/source-items") {Method = Method.DELETE};
+			request.AddJsonBody(new { sourceItems = items });
+
+			return ExecuteAsync(request);
 		}
 	}
 }
